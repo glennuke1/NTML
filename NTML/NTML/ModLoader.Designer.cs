695505393e6b440d81b177742c81959f02d404d1
glennuke1 <132171380+glennuke1@users.noreply.github.com>
2023-06-20 18:35:22 +0300

    Add files via upload

---
+++ NTML/NTML/ModLoader.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace NTML
+{
+    partial class ModLoader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModLoader));
+            this.buttonSelectModsFolder = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonInstallMod = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.Logo = new System.Windows.Forms.Button();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.Minimize = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Close = new System.Windows.Forms.Button();
+            this.TopPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSelectModsFolder
+            // 
+            this.buttonSelectModsFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.buttonSelectModsFolder.FlatAppearance.BorderSize = 0;
+            this.buttonSelectModsFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelectModsFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.buttonSelectModsFolder.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonSelectModsFolder.Location = new System.Drawing.Point(118, 51);
+            this.buttonSelectModsFolder.Name = "buttonSelectModsFolder";
+            this.buttonSelectModsFolder.Size = new System.Drawing.Size(100, 30);
+            this.buttonSelectModsFolder.TabIndex = 2;
+            this.buttonSelectModsFolder.TabStop = false;
+            this.buttonSelectModsFolder.Text = "Mods Folder";
+            this.buttonSelectModsFolder.UseVisualStyleBackColor = false;
+            this.buttonSelectModsFolder.Click += new System.EventHandler(this.buttonSelectModsFolder_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(729, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(119, 364);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(224, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.buttonRefresh.FlatAppearance.BorderSize = 0;
+            this.buttonRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRefresh.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonRefresh.Location = new System.Drawing.Point(729, 51);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(119, 23);
+            this.buttonRefresh.TabIndex = 9;
+            this.buttonRefresh.TabStop = false;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = false;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonInstallMod
+            // 
+            this.buttonInstallMod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.buttonInstallMod.FlatAppearance.BorderSize = 0;
+            this.buttonInstallMod.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInstallMod.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.buttonInstallMod.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonInstallMod.Location = new System.Drawing.Point(12, 51);
+            this.buttonInstallMod.Name = "buttonInstallMod";
+            this.buttonInstallMod.Size = new System.Drawing.Size(100, 30);
+            this.buttonInstallMod.TabIndex = 10;
+            this.buttonInstallMod.TabStop = false;
+            this.buttonInstallMod.Text = "Install";
+            this.buttonInstallMod.UseVisualStyleBackColor = false;
+            this.buttonInstallMod.Click += new System.EventHandler(this.buttonInstallMod_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.comboBox1.CausesValidation = false;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Latest",
+            "Legacy 2020",
+            "Legacy 2019"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.TabStop = false;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Controls.Add(this.Logo);
+            this.TopPanel.Controls.Add(this.Minimize);
+            this.TopPanel.Controls.Add(this.Close);
+            this.TopPanel.Location = new System.Drawing.Point(-1, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(862, 45);
+            this.TopPanel.TabIndex = 12;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            // 
+            // Logo
+            // 
+            this.Logo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.Logo.FlatAppearance.BorderSize = 0;
+            this.Logo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Logo.ImageKey = "icon-removebg-preview.png";
+            this.Logo.ImageList = this.imageList2;
+            this.Logo.Location = new System.Drawing.Point(3, 3);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(40, 40);
+            this.Logo.TabIndex = 2;
+            this.Logo.TabStop = false;
+            this.Logo.UseVisualStyleBackColor = false;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "icon-removebg-preview.png");
+            // 
+            // Minimize
+            // 
+            this.Minimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.Minimize.FlatAppearance.BorderSize = 0;
+            this.Minimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.Minimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Minimize.ImageKey = "minimize-sign.png";
+            this.Minimize.ImageList = this.imageList1;
+            this.Minimize.Location = new System.Drawing.Point(773, 3);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(40, 40);
+            this.Minimize.TabIndex = 1;
+            this.Minimize.TabStop = false;
+            this.Minimize.UseVisualStyleBackColor = false;
+            this.Minimize.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "close.png");
+            this.imageList1.Images.SetKeyName(1, "minimize-sign.png");
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.Close.FlatAppearance.BorderSize = 0;
+            this.Close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.Close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Close.ImageKey = "close.png";
+            this.Close.ImageList = this.imageList1;
+            this.Close.Location = new System.Drawing.Point(819, 3);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(40, 40);
+            this.Close.TabIndex = 0;
+            this.Close.TabStop = false;
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ModLoader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(860, 450);
+            this.Controls.Add(this.buttonSelectModsFolder);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.buttonInstallMod);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ModLoader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NTML";
+            this.Load += new System.EventHandler(this.ModLoader_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ModLoader_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ModLoader_MouseMove);
+            this.TopPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSelectModsFolder;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonInstallMod;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Button Minimize;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Logo;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ NTML/NTML/ModLoader.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace NTML
+{
+    public partial class ModLoader : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+        private string _modsPath = "";
+
+        public ModLoader()
+        {
+            InitializeComponent();
+        }
+
+        public static void PopulateListBox(ListBox lsb, string Folder, string FileType)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            FileInfo[] Files = dinfo.GetFiles(FileType);
+            foreach (FileInfo file in Files)
+            {
+                lsb.Items.Add(file.Name);
+            }
+        }
+
+        private void ModLoader_Load(object sender, EventArgs e)
+        {
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 5, 5));
+
+            foreach (Button button in GetAll(this, typeof(Button)))
+            {
+                button.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button.Width, button.Height, 2, 2));
+            }
+
+            string configFilePath = Path.Combine(Application.UserAppDataPath, "config.txt");
+            if (File.Exists(configFilePath))
+            {
+                _modsPath = File.ReadAllText(configFilePath);
+            }
+            else
+            {
+                _modsPath = Path.Combine(Application.StartupPath, "Mods");
+            }
+            label1.Text = _modsPath;
+
+            if (!Directory.Exists(_modsPath))
+            {
+                Directory.CreateDirectory(_modsPath);
+            }
+
+            listBox1.Items.Clear();
+            PopulateListBox(listBox1, _modsPath, "*.dll");
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        public IEnumerable<Control> GetAll(Control control, Type type)
+        {
+            var controls = control.Controls.Cast<Control>();
+
+            return controls.SelectMany(ctrl => GetAll(ctrl, type))
+                                      .Concat(controls)
+                                      .Where(c => c.GetType() == type);
+        }
+
+        private void buttonSelectModsFolder_Click(object sender, EventArgs e)
+        {
+            var folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.SelectedPath = _modsPath;
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                _modsPath = folderBrowserDialog.SelectedPath;
+
+                listBox1.Items.Clear();
+                label1.Text = _modsPath;
+
+                string configFilePath = Path.Combine(Application.UserAppDataPath, "config.txt");
+                File.WriteAllText(configFilePath, _modsPath);
+            }
+        }
+
+        private void buttonInstallMod_Click(object sender, EventArgs e)
+        {
+            string filePath = Directory.GetParent(_modsPath) + "/Managed/Assembly-CSharp.dll";
+            string filePath2 = Directory.GetParent(_modsPath) + "/Managed/NTLoader.dll";
+
+            string fileUrl = "https://drive.google.com/u/0/uc?id=1Ly9LmyBxYUl0uAWj_jgQM1EH-5VKOyw4&export=download"; //Assembly-CSharp.dll https://drive.google.com/u/0/uc?id=1Ly9LmyBxYUl0uAWj_jgQM1EH-5VKOyw4&export=download
+            string fileUrl2 = "https://drive.google.com/u/0/uc?id=1jYCbEYdUgeNUyfe725gYiePefYV5YwJh&export=download"; //NTLoader.dll https://drive.google.com/u/0/uc?id=1jYCbEYdUgeNUyfe725gYiePefYV5YwJh&export=download
+
+            if (!File.Exists(filePath2))
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(fileUrl2, filePath2);
+                }
+            }
+
+            try
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(fileUrl, filePath);
+                    }
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    string legacyFileUrl = "https://drive.google.com/u/0/uc?id=1RPCwmmnR6BQ1oe9Dx9cdXvv_ITUfD1tZ&export=download";
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(legacyFileUrl, filePath);
+                    }
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    string legacyFileUrl = "https://drive.google.com/u/0/uc?id=18tROArgWoEQSZq2hgOJjUa5ge0BR7h3V&export=download";
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(legacyFileUrl, filePath);
+                    }
+                }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+            MessageBox.Show("Install successful");
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            PopulateListBox(listBox1, _modsPath, "*.dll");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        Point lastPoint;
+
+        private void ModLoader_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void ModLoader_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - lastPoint.X;
+                Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void TopPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - lastPoint.X;
+                Top += e.Y - lastPoint.Y;
+            }
+        }
+    }
+}
